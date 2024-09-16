183338a176ff614c8567b165fe94a5361543a277
Santiago Pinto <Santiago.pinto12399@gmail.com>
2024-09-15 21:18:18 -0500

    Removed EnemyMovement modifeid LargeEnemyMovement so that both enemy types can use it by adding bool is large to modify movement renamed LargeEnemyMovement -> EnemyMovement

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,73 +4,128 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+	// Component for physics calculations
     private Rigidbody2D physicsBody;
-	private Vector2 playerPosition;
-	private float radian, randSpeed, evenmoremath;
+	// Store players position and initial position for gameobject
+	private Vector2 targetPos, initPos;
+	// enemy rotation speed
     public float speed = 5;
 
-	// Use this for initialization
+	public bool isLarge;
+	// player object
 	private GameObject playerObj = null;
+
+	// runs at when program is initialized
 	void Start(){
+		// finds the player object by looking for tag
 		playerObj = GameObject.FindGameObjectWithTag("Player");
 
-		randSpeed = UnityEngine.Random.Range(0.1f, 1f);
+		// stores the initial position of the game object
+        initPos = transform.position;
+		// Moves the position of enemy to avoid some warning messages
+        transform.position = new Vector3(transform.position.x-1, transform.position.y+1, 0);
+
+		// Get the Rigidbody2D component attached to this GameObject
 		physicsBody = GetComponent<Rigidbody2D>();
 	}
-	//12345678901234567890123456789012345678901234567890123456789012345678901234567890
-	//I dont knwo what im doing anymore
+
 	// Update is called once per frame
 	void Update () {
 
+		// Stores player's current position 
 		float[] player = {playerObj.transform.position.x,
 						  playerObj.transform.position.y};
 
+		// Store enemy's current position
 		float[] enemy = {this.transform.position.x, this.transform.position.y};
 
-		float[] temp = angle();
-		evenmoremath = rotation(player, enemy, temp[2]);
-		targetting(evenmoremath);
+		// Store enemy's initial position
+        float[] init2 = {initPos.x, initPos.y};
 
-		playerPosition = new Vector2(temp[0], temp[1]);
+		float[] temp;
+
+		if(isLarge){
+			// Check if player is currently within a radius of X
+			// If the player is within the given radius, will chase player
+			// If not it will return to its Origin point
+			if(InRange(player, enemy)){
+				// Calls angle to find angle of rotation to player
+				temp = angle(player, enemy);
+			}else{
+				// Calls angle to find angle of rotation to Origin point
+				temp = angle(init2, enemy);
+			}
+		}else{
+			temp = angle(player, enemy);
+		}
+
+		//Rotates towards Origin
+        targetting(temp[2]);
+
+		// Moves towards Origin point
+        targetPos = new Vector2(temp[0], temp[1]);
 	}
 
-	public float[] angle(){
-		float xvalue = playerObj.transform.position.x - 
-						this.transform.position.x;
-		float yvalue = playerObj.transform.position.y - 
-						this.transform.position.y;
+	/*
+	Takes two values 
+	Player array which has current X and Y pos
+	Enemy array which has current X and Y pos
+	*/
+	public float[] angle(float[] player, float[] enemy){
+		
+		// Calculates difference between x and Y values 
+		float xvalue = player[0] - enemy[0];
+		float yvalue = player[1] - enemy[1];
 
-		radian = MathF.Atan(yvalue/xvalue);
+		// Calculate the angle in radians using Atan
+		// And converts radians back to degrees
+		float radian = MathF.Atan(yvalue/xvalue)*Mathf.Rad2Deg;
 
+		// Adjust rotation based on the relative position of player and enemy
+		if(player[0] < enemy[0] && player[1] < enemy[1]){
+			radian+= 90;
+		}else if(player[0] > enemy[0] && player[1] < enemy[1]){
+			radian-= 90;
+		}else if(player[0] > enemy[0] && player[1] > enemy[1]){
+			radian-= 90;
+		}else{
+			radian+= 90;
+		}
+
+		// return the difference Between X, Y, and the Degree to rotate
 		float[] output = {xvalue, yvalue, radian};
-
 		return output;
 	}
 
-	public float rotation(float[] player, float[] enemy, float radian){
+	// Is used to determin wether or not player is within chasing range
+	// By calculating distance between two objects, using Dot product
+    public bool InRange(float[] player, float[] enemy){
+		// Calculate distance using Pythagorean theorem
+        float xvalue = Mathf.Pow(player[0]-enemy[0], 2);
+        float yvalue = Mathf.Pow(player[1]-enemy[1], 2);
+
+		// Get the square root of the sum to find the distance
+        float distance = Mathf.Sqrt(xvalue+yvalue);
+
+		// Return true if the distance is less than or equal to 5 units
+        if (distance <= 5){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+	// Rotates Gameobject Towards Given Target
+	public void targetting(float Rotation){
 	
-		if(player[0] < enemy[0] && player[1] < enemy[1]){
-			evenmoremath = ((float)radian*Mathf.Rad2Deg) + 90;
-		}else if(player[0] > enemy[0] && player[1] < enemy[1]){
-			
-			evenmoremath = ((float)radian*Mathf.Rad2Deg) - 90;
-		}else if(player[0] > enemy[0] && player[1] > enemy[1]){
-
-			evenmoremath = ((float)radian*Mathf.Rad2Deg) - 90;
-		}else{
-			evenmoremath =  ((float)radian*Mathf.Rad2Deg) + 90;
-		}
-
-		return evenmoremath;
-	}
-
-
-	public void targetting(float plaerRotation){
-
-		Quaternion target = Quaternion.Euler(0, 0, plaerRotation);
+		// Create a quaternion to rotate the enemy towards the target angle
+		Quaternion target = Quaternion.Euler(0, 0, Rotation);
+		// Smoothly rotate the enemy towards the target using Slerp
 		transform.rotation = Quaternion.Slerp(transform.rotation, 
 											  target, Time.deltaTime*speed);
-                                              
+	
+		/* ---To be implemented later---
+
 		float Rotation;
 		if (transform.eulerAngles.z <= 180f){
 			Rotation = transform.eulerAngles.z;
@@ -78,14 +133,17 @@ public class EnemyMovement : MonoBehaviour
 			Rotation = transform.eulerAngles.z - 360f;
 		}
 
-		if(Rotation <= plaerRotation+5f && Rotation >= plaerRotation-5f){
+		if(Rotation <= Rotation+5f && Rotation >= Rotation-5f){
 			//Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAaaAAAAAAAAAAAAAAAaaaaAAAA");
 		}
+		*/
 
 	}
 
+	// Called at a fixed time interval
 	private void FixedUpdate() 
-    {
-        physicsBody.AddForce(randSpeed * playerPosition);
+    {   
+		// Apply force to move towards targetted position
+        physicsBody.AddForce(targetPos);
     }
 }
