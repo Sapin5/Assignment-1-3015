3449e0bc1c1d3ef6dcfa3a127ea07d0670355689
Santiago Pinto <spinto@rrc.ca>
2024-09-27 13:33:55 -0500

    fix

--- Assets/Scripts/EnemyDeath.cs
+++ Assets/Scripts/EnemyDeath.cs
@@ -3,6 +3,9 @@ public class EnemyDeath : MonoBehaviour
 {
     // Explosion prefab hodler 
     public GameObject explosionGo;
+    public int healthPoints;
+
+    public bool isBoss = false;
     
     // Detects Collsions in order to destroy itself 
     private void OnCollisionEnter2D(Collision2D collision){
@@ -13,16 +16,32 @@ public class EnemyDeath : MonoBehaviour
         If it collides with a bullet it will remove itself
         but not add an explosion as the bullet is already creating one
         */
-        if(collision.gameObject.CompareTag("Player") || 
-        collision.gameObject.CompareTag("Enemy")) {
-            // Removes game object
-            Destroy(gameObject);
-            // Spawns an explosion
-            Instantiate(explosionGo, transform.position, transform.rotation);
-        }else if(collision.gameObject.CompareTag("Bullet"))
-        {   
-            // Remoces game object
-            Destroy(gameObject);
+
+        if(isBoss){
+            // If the object it collided with is a bullet lowers HP by 1
+            if(collision.gameObject.CompareTag("Bullet")){
+                healthPoints-=1;
+            }
+
+            // Destroys gameobject (boss) once HP hits 0
+            if(healthPoints == 0){
+                Destroy(gameObject);
+                StartGame.bossCount-=1;
+                // Creates explosion game object
+                Instantiate(explosionGo, transform.position, transform.rotation);
+            }
+        }else{
+            if(collision.gameObject.CompareTag("Player") || 
+                collision.gameObject.CompareTag("Enemy")) {
+                    // Removes game object
+                    Destroy(gameObject);
+                    // Spawns an explosion
+                    Instantiate(explosionGo, transform.position, transform.rotation);
+            }else if(collision.gameObject.CompareTag("Bullet")){   
+                // Removes game object
+                Destroy(gameObject);
+            }
+
         }
     }
     
